37f34ee467c26e98c105d09f83fe8388f5d05655
10varun17 <17varun.rayamajhi@gmail.com>
2024-08-22 16:06:29 -0400

    Add static_circle_obstacle and control_barrier_function

--- cbf.py
+++ cbf.py
@@ -1,6 +1,21 @@
 import numpy as np
 from jax import grad
 
+class CircleObstacle:
+    def __init__(self, x, y, r):
+        self._x = x
+        self._y = y
+        self._radius = r
+
+    def get_x(self):
+        return self._x
+    
+    def get_y(self):
+        return self._y
+    
+    def get_radius(self):
+        return self._radius
+
 def f(q):
     return np.zeros((3,1))
 
@@ -21,6 +36,20 @@ def control_affine_dynamics(q, u):
 
 def control_law(q, q_g):
     return q_g[:2] - q[:2]
+
+def non_linear_transform(delx, dely, x_r, curr_yaw):
+    """
+    Receives x and y velocity and returns the corresponding linear and angular velocity
+    """
+    v = (1 / x_r) * (x_r * np.cos(curr_yaw) * delx + x_r * np.sin(curr_yaw) * dely)
+    omega = (1 / x_r) * (-1 * np.sin(curr_yaw) * delx + np.cos(curr_yaw) * dely)
+
+    return v, omega
+
+def control_barrier_function(q, obs : CircleObstacle):
+    xo, yo, ro = obs.get_x(), obs.get_y(), obs.get_radius()
+    x, y = q[0, 0], q[1, 0]
+    return (x - xo)**2 + (y - yo)**2 - ro**2
 
 def cbf_safe_dist(x_diff: np.ndarray, d_safe):
     """
