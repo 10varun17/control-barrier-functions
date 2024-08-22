8d1089acbea08e141eae413e4195650752144fad
10varun17 <17varun.rayamajhi@gmail.com>
2024-08-22 17:28:51 -0400

    Compute lie derivatives

--- cbf.py
+++ cbf.py
@@ -1,5 +1,5 @@
 import numpy as np
-from jax import grad
+import casadi as ca
 
 class CircleObstacle:
     def __init__(self, x, y, r):
@@ -37,14 +37,14 @@ def control_affine_dynamics(q, u):
 def control_law(q, q_g):
     return q_g[:2] - q[:2]
 
-def non_linear_transform(delx, dely, x_r, curr_yaw):
+def nominal_controller(delx, dely, x_r, curr_yaw):
     """
     Receives x and y velocity and returns the corresponding linear and angular velocity
     """
     v = (1 / x_r) * (x_r * np.cos(curr_yaw) * delx + x_r * np.sin(curr_yaw) * dely)
     omega = (1 / x_r) * (-1 * np.sin(curr_yaw) * delx + np.cos(curr_yaw) * dely)
 
-    return v, omega
+    return np.array([v, omega]).reshape(2,1)
 
 def compute_h(q, obs : CircleObstacle):
     xo, yo, ro = obs.get_x(), obs.get_y(), obs.get_radius()
@@ -55,44 +55,49 @@ def compute_hdot(q, qdot, obs: CircleObstacle):
     qo = np.array([obs.get_x(), obs.get_y(), obs.get_radius()]).reshape(3,1)
     return 2 * (q[:2, 0] - qo[:2, 0]).T @ qdot[:2, 0]
 
-def cbf_safe_dist(x_diff: np.ndarray, d_safe):
-    """
-    Returns the control barrier function for maintaining safe distance
+def compute_lfh(q, obs):
+    xo, yo, ro = obs.get_x(), obs.get_y(), obs.get_radius()
+    x, y, phi = q[0, 0], q[1, 0], q[2, 0]
+    h = (x - xo)**2 + (y - yo)**2 - ro**2
+    dh_dq = np.array([
+        [2*(x - xo)],
+        [2*(y - yo)],
+        [0]
+    ])
+    f_q = f(q)
+    return dh_dq[:, 0].T @ f_q[:,0]
 
-    Args:
-        x_diff: the relative state of two agents (x_diff = x_i - x_j)
-        d_safe: the safety distance between agents i and j
-    """
-    return -x_diff.T @ x_diff + d_safe**2 
+def compute_lgh(q, obs):
+    xo, yo, ro = obs.get_x(), obs.get_y(), obs.get_radius()
+    x, y, phi = q[0, 0], q[1, 0], q[2, 0]
+    h = (x - xo)**2 + (y - yo)**2 - ro**2
 
-def grad_f(f, params, argnums=0):
-    """
-    Computes the gradient of f with respect to variable at argnums in f and evaluetes at (x_diff, d_safe)
-    """
-    params_tuple = tuple(params)
-    grad_f = grad(f, argnums)
-    grad_value = grad_f(*params_tuple)
-    return np.array(grad_value)
+    dh_dq = np.array([
+        [2*(x - xo)],
+        [2*(y - yo)],
+        [0]
+    ])
+    g_q = g(q)
+    g1_q = g_q[:, 0]
+    g2_q = g_q[:, 1]
 
-def lie_derivative(h_func, f_func, params, argnums=0):
-    grad_h = grad_f(h_func, params, argnums)
-    f_x = f_func(params[argnums])
-    return grad_h.T @ f_x
+    lg_h = np.zeros((2,))
+    lg_h[0] = dh_dq[:, 0].T @ g1_q
+    lg_h[1] = dh_dq[:, 0].T @ g2_q
 
-# x_agent = np.array([1.0, -2.0])
-# u_agent = np.array([-0.4, 0.2])
+    return lg_h
 
-# x_obs = np.array([2.0, 3.0])
-# u_obs = np.array([-0.3, 0.1])
+def cbf_qp_controller(u_nom, h, hdot, gamma=1.0):
+    opti = ca.Opti()
+    u_safe = opti.variable(2,1)
+    opti.subject_to(hdot + gamma*h >= 0)
+    opti.set_initial(u_safe, u_nom)
+    cost_func = (u_safe - u_nom).T @ (u_safe - u_nom)
+    opti.minimize(cost_func)
+    option = {"verbose": False, "ipopt.print_level": 0, "print_time": 0}
+    opti.solver("ipopt", option)
+    sol = opti.solve()
+    u_safe_opti = sol.value(u_safe)
+    return u_safe_opti
 
-# x_diff = x_agent - x_obs
-# x_dot = control_affine_dynamics(x_agent, u_agent) - control_affine_dynamics(x_obs, u_obs) # difference of the dynamics.. for 2d si, it's u_agent - u_obs
-
-# agent_rad = 0.5
-# h = cbf_safe_dist(x_diff, agent_rad) # control barrier function h = norm**2 - dist**2
-
-# grad_h = grad_f(cbf_safe_dist, params=[x_diff, agent_rad])
-# lg_h = grad_h.T@x_dot
-
-# print(lie_derivative(cbf_safe_dist, f, params=[x_diff, agent_rad]))
 
