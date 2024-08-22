1e8629658f171c6d3cd2dafd8b797c5f2f3cb9e5
10varun17 <17varun.rayamajhi@gmail.com>
2024-08-22 15:49:39 -0400

    Add control affine dynamics and control aw

--- cbf.py
+++ cbf.py
@@ -1,17 +1,26 @@
 import numpy as np
 from jax import grad
 
-def f(x):
-    return np.zeros(2)
+def f(q):
+    return np.zeros((3,1))
 
-def g(x):
-    return np.eye(2)
+def g(q):
+    phi = q[2, 0]
+    g_q = np.array([
+        [np.cos(phi), 0],
+        [np.sin(phi), 0],
+        [0, 1]
+    ])
+    return g_q
 
-def dynamics(x, u):
+def control_affine_dynamics(q, u):
     """
     Creates the control affine dynamics for single integrator.
     """
-    return f(x) + g(x) @ u
+    return f(q) + g(q) @ u
+
+def control_law(q, q_g):
+    return q_g[:2] - q[:2]
 
 def cbf_safe_dist(x_diff: np.ndarray, d_safe):
     """
@@ -37,20 +46,20 @@ def lie_derivative(h_func, f_func, params, argnums=0):
     f_x = f_func(params[argnums])
     return grad_h.T @ f_x
 
-x_agent = np.array([1.0, -2.0])
-u_agent = np.array([-0.4, 0.2])
+# x_agent = np.array([1.0, -2.0])
+# u_agent = np.array([-0.4, 0.2])
 
-x_obs = np.array([2.0, 3.0])
-u_obs = np.array([-0.3, 0.1])
+# x_obs = np.array([2.0, 3.0])
+# u_obs = np.array([-0.3, 0.1])
 
-x_diff = x_agent - x_obs
-x_dot = dynamics(x_agent, u_agent) - dynamics(x_obs, u_obs) # difference of the dynamics.. for 2d si, it's u_agent - u_obs
+# x_diff = x_agent - x_obs
+# x_dot = control_affine_dynamics(x_agent, u_agent) - control_affine_dynamics(x_obs, u_obs) # difference of the dynamics.. for 2d si, it's u_agent - u_obs
 
-agent_rad = 0.5
-h = cbf_safe_dist(x_diff, agent_rad) # control barrier function h = norm**2 - dist**2
+# agent_rad = 0.5
+# h = cbf_safe_dist(x_diff, agent_rad) # control barrier function h = norm**2 - dist**2
 
-grad_h = grad_f(cbf_safe_dist, params=[x_diff, agent_rad])
-lg_h = grad_h.T@x_dot
+# grad_h = grad_f(cbf_safe_dist, params=[x_diff, agent_rad])
+# lg_h = grad_h.T@x_dot
 
-print(lie_derivative(cbf_safe_dist, f, params=[x_diff, agent_rad]))
+# print(lie_derivative(cbf_safe_dist, f, params=[x_diff, agent_rad]))
 
